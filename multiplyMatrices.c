680c643c4bfe46e5b1989cc7befbb0f1773039eb
Seth Stalley <sethstalley@gmail.com>
2016-09-10 16:30:11 -0600

    single process matrix mul done

--- multiplyMatrices.c
+++ multiplyMatrices.c
@@ -2,43 +2,57 @@
 
 int **researveMemoryForMatrix(int **matrix, int n, int m);
 void freeMatrix(int **matrix, int rows);
+void printMatrix(int **matrix, int rows, int columns);
 
 //Loads matrix from file into a 2D array, array must be freed after use
-int **loadMatrix(int **ptr_Matrix, char *name, int *rows) {
+int **loadMatrix(int **ptr_Matrix, char *name, int *rows, int *columns) {
     FILE *matrixFile;
-    int m, n;
 
     //open file for reading
     matrixFile = openFile(name, "r");
 
     //read matrices size
-    fscanf(matrixFile, "%d %d\n",&m,&n);
-    ptr_Matrix = researveMemoryForMatrix(ptr_Matrix, m,n);
+    fscanf(matrixFile, "%d %d\n",rows,columns);
+    ptr_Matrix = researveMemoryForMatrix(ptr_Matrix, *rows, *columns);
 
     //fill out matrix
-    for(int i=0; i<m; i++) {
-        for(int j=0; j<n ; j++) {
+    for(int i=0; i < *rows; i++) {
+        for(int j=0; j < *columns ; j++) {
             fscanf(matrixFile, "%d", &ptr_Matrix[i][j] );
         }
     }
 
     //close file
     fclose(matrixFile);
-    *rows = m;
     return ptr_Matrix;
 }
 
 void singleProcessMul() {
-    int **matrixA = NULL, **matrixB = NULL;
-    int rowsA, rowsB;
+    int **matrixA = NULL, **matrixB = NULL, **matrixC;
+    int rowsA, columnsA, rowsB, columnsB;
 
     //Fills our matrixA and matrixB pointers from FILE_A & FILE_B
-    matrixA = loadMatrix(matrixA, FILE_A, &rowsA);
-    matrixB = loadMatrix(matrixB, FILE_B, &rowsB);
+    matrixA = loadMatrix(matrixA, FILE_A, &rowsA, &columnsA);
+    matrixB = loadMatrix(matrixB, FILE_B, &rowsB, &columnsB);
+
+    //reserver memory for our 'C'' (result) matrix
+    matrixC = researveMemoryForMatrix(matrixC, rowsA, columnsB);
+
+    //multiply
+    for (int i=0; i< rowsA; i++) {
+        for (int j=0; j < columnsB; j++) {
+            int cellValue = 0;
+            for (int z=0; z < rowsB; z++) {
+                cellValue += matrixA[i][z] * matrixB[z][j];
+            }
+            matrixC[i][j] = cellValue;
+        }
+    }
 
     //free our memory
     freeMatrix(matrixA, rowsA);
     freeMatrix(matrixB, rowsB);
+    freeMatrix(matrixC, rowsA);
 }
 
 //reserver memory for a int matrix size N * M
@@ -58,6 +72,15 @@ void freeMatrix(int **matrix, int rows) {
             free(matrix[i]);
             
         free(matrix);
+    }
+}
+
+void printMatrix(int **matrix, int rows, int columns) {
+    for (int i=0; i<rows; i++) {
+        for (int j=0; j<columns; j++) {
+            printf("%d\t", matrix[i][j]);
+        }
+        printf("\n");
     }
 }
 
